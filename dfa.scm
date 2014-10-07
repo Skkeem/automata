2824975ed22488c4758f0c29657ac0c5337d13cc
Skkeem <Skkeem@users.noreply.github.com>
2014-10-08 00:02:03 +0900

    two

--- dfa.scm
+++ dfa.scm
@@ -7,13 +7,13 @@
 (define alphabet (read))
 
 ;transition function을 입력 받음.
-(display "3. transition function을 입력하세요. ex) ((e 0 0) (e 1 1) (1 2 0) ...)\n")
-;(display "3. transition function을 입력하세요. ex) ((e ((0 0) (1 1))) (1 ((0 3) (1 4) (2 0))) ...)\n")
+;(display "3. transition function을 입력하세요. ex) ((e 0 0) (e 1 1) (1 2 0) ...)\n")
+(display "3. transition function을 입력하세요. ex) ((e ((0 0) (1 1))) (1 ((0 3) (1 4) (2 0))) ...)\n")
 (define tf (read))
 
 ;initial state를 입력 바음.
 (display "4. initial state를 입력하세요. ex) e\n")
-(define state (read))
+(define init (read))
 
 ;final state들을 입력 바음.
 (display "5. final state들을 입력하세요. ex) (0)\n")
@@ -28,14 +28,54 @@
 
 ;;tf를 미리 작업해놓을까? 어떤 ds를 사용하지
 ;;((state1 ((a1 q1) (a2 q2) (a3 q3) ... )) (state2 ((a1 q1) (a2 q2) ...)))
-;;(define (transit str)
-;;  (if (empty? str)
-;;      state
-;;      (if (not (in (car str) q))
-;;          'deadstate
-;;          (begin (set! state next)
-;;                 (transit (cdr str))))))
+;;
+(define (search key dic)
+  (if (null? dic)
+    #f
+    (let ((entry (car dic)))
+      (if (eq? key (car entry))
+        (cadr entry)
+        (search key (cdr dic))))))
+  
+;(define (search1 q tf_)
+;  (if (null? tf_)
+;    #f
+;    (let ((tf_elem (car tf_)))
+;        (if (eq? q (car tf_elem))
+;            (cadr tf_elem)
+;            (search1 q (cdr tf_))))))
+
+;(define (search2 a qtf)
+;  (if (null? qtf)
+;    #f
+;    (let ((qtf_elem (car qtf)))
+;        (if (eq? a (car qtf_elem))
+;            (cadr qtf_elem)
+;            (search2 a (cdr qtf))))))
+;;
+(define (transit1 q a)
+  (let ((qtf (search q tf)))
+    (if qtf
+      (search a (cdr qtf))
+      #f)))
+;;
+;;
+(define (transit curr str)
+  (if (empty? str)
+    curr
+    (let ((next (transit1 curr (car str))))
+      (if next
+        (transit next (cdr str))
+        'deadstate))))
 ;;              
-;;(if (in (transit input) f)
+;(define (transit curr str)
+;  (if (empty? str)
+;    curr
+;    (let ((next (transit1 curr (car str))))
+;      (if next
+;        (transit next (cdr str))
+;        'deadstate))))
+;;
+;;(if (in (transit init input) f)
 ;;    (display "네.\n")
 ;;    (display "아니오.\n"))
