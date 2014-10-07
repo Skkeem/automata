dbb93e2780518d49e1da579ddcb20c1d9130255f
Skkeem <Skkeem@users.noreply.github.com>
2014-10-08 00:02:21 +0900

    three

--- dfa.scm
+++ dfa.scm
@@ -1,34 +1,26 @@
 ;state들을 입력 받음.
 (display "1. state들을 입력하세요. ex) (e 0 1 2 3 4)\n")
-(define q (read))
+;(define q (read))
 
 ;alphabet을 입력 받음.
 (display "2. alphabet을 입력하세요. ex) (0 1 2)\n")
-(define alphabet (read))
+;(define alphabet (read))
 
 ;transition function을 입력 받음.
-;(display "3. transition function을 입력하세요. ex) ((e 0 0) (e 1 1) (1 2 0) ...)\n")
 (display "3. transition function을 입력하세요. ex) ((e ((0 0) (1 1))) (1 ((0 3) (1 4) (2 0))) ...)\n")
-(define tf (read))
+;(define tf (read))
 
 ;initial state를 입력 바음.
 (display "4. initial state를 입력하세요. ex) e\n")
-(define init (read))
+;(define init (read))
 
 ;final state들을 입력 바음.
 (display "5. final state들을 입력하세요. ex) (0)\n")
-(define f (read))
+;(define f (read))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(display "string을 입력하세요. ex) 120\n")
-(define input (read))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;tf를 미리 작업해놓을까? 어떤 ds를 사용하지
-;;((state1 ((a1 q1) (a2 q2) (a3 q3) ... )) (state2 ((a1 q1) (a2 q2) ...)))
-;;
 (define (search key dic)
   (if (null? dic)
     #f
@@ -36,46 +28,44 @@
       (if (eq? key (car entry))
         (cadr entry)
         (search key (cdr dic))))))
-  
-;(define (search1 q tf_)
-;  (if (null? tf_)
-;    #f
-;    (let ((tf_elem (car tf_)))
-;        (if (eq? q (car tf_elem))
-;            (cadr tf_elem)
-;            (search1 q (cdr tf_))))))
-
-;(define (search2 a qtf)
-;  (if (null? qtf)
-;    #f
-;    (let ((qtf_elem (car qtf)))
-;        (if (eq? a (car qtf_elem))
-;            (cadr qtf_elem)
-;            (search2 a (cdr qtf))))))
-;;
+ 
 (define (transit1 q a)
   (let ((qtf (search q tf)))
     (if qtf
       (search a (cdr qtf))
       #f)))
-;;
-;;
-(define (transit curr str)
-  (if (empty? str)
-    curr
-    (let ((next (transit1 curr (car str))))
-      (if next
-        (transit next (cdr str))
-        'deadstate))))
-;;              
-;(define (transit curr str)
-;  (if (empty? str)
-;    curr
-;    (let ((next (transit1 curr (car str))))
-;      (if next
-;        (transit next (cdr str))
-;        'deadstate))))
-;;
-;;(if (in (transit init input) f)
-;;    (display "네.\n")
-;;    (display "아니오.\n"))
+
+(define (transit q w)
+  (if (null? w)
+    q
+    (if q ; dead state is represented by #f 
+        (transit (transit1 q (car w)) (cdr w))
+        #f)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (char->wanted c)
+  (if (or (char<=? #\0 c) (char<=? c #\9))
+    (- (char->integer c) 48)
+    ((string->symbol (list->string (list c))))))
+
+(define (input->list in)
+;  (map char->wanted
+       (string->list (if (number? in)
+                       (number->string in)
+                       (symbol->string in))))
+
+;(define (loop)
+;  (display "string을 입력하세요. ex) 120\n")
+;  (let ((result (transit init (input->list (read)))))
+;    (if (and result (in result f))
+;      (display "네.\n")
+;      (display "아니오.\n")))
+;  (loop))
+
+;(loop)
+(define (loop)
+    (display (input->list (read)))
+    (loop))
+(loop)
